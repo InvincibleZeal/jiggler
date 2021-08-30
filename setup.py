0e4a5d3b600a923207082c434414a78bff773c98
Ritesh Ganjewala <ritesh.ganjewala17@gmail.com>
2021-08-30 14:33:01 +0530

    :wrench: (setup.py) Cover more python3 versions

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="jiggler",
-    version="0.0.1",
+    version="0.0.2",
     author="Ritesh Ganjewala",
     author_email="ritesh.ganjewala17@gmail.com",
     license="MIT",
@@ -15,12 +15,24 @@ setup(
     url="https://github.com/InvincibleZeal/jiggler",
     py_modules=["jiggler"],
     packages=find_packages(),
-    install_requires=["click>=7", "pynput>=1"],
-    python_requires=">=3.7",
+    install_requires=["click >= 7", "pynput >= 1"],
+    python_requires=">=3",
     classifiers=[
-        "Programming Language :: Python :: 3.9",
+        "Intended Audience :: End Users/Desktop",
         "Topic :: Utilities",
+        "Topic :: Terminals",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows :: Windows NT/2000",
+        "Operating System :: POSIX",
         "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.9",
     ],
     entry_points="""
         [console_scripts]
